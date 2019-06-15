8d3e90f5a148c2cab18b7432c9a6beb61ed7a55a
Bryce Morgan <bamorgan13@gmail.com>
2019-06-14 20:40:51 -0400

    Create UserType and RootQueryType schema

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express')
 const app = express()
 const db = require('./config/keys').mongoURI
 const bodyParser = require('body-parser')
+const expressGraphQL = require('express-graphql')
 
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log('Connected to MongoDB successfully'))
 	.catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
 app.use(bodyParser.json())
+
+app.use('/graphql', expressGraphQL({ graphiql: true }))
+
 app.listen(5000, () => console.log('Server is running on port 5000'))
