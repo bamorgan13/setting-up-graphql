8a759228a16c62ae4b0977beb312b56cbffedc93
Bryce Morgan <bamorgan13@gmail.com>
2019-06-14 20:00:32 -0400

    Create mongodb atlas db and connect app

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const db = require('./config/keys').mongoURI
 const bodyParser = require('body-parser')
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('Connected to MongoDB successfully'))
+	.catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
